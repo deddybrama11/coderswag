b614d55226b7a752ca18fd9d6c75e329b568db34
dedybramayadi <dedybramayadi@gmail.com>
2020-10-28 23:55:35 +0700

    recyclerview for product

--- app/src/main/java/com/bram/coderswah/controller/ProductsActivity.kt
+++ app/src/main/java/com/bram/coderswah/controller/ProductsActivity.kt
@@ -1,16 +1,39 @@
 package com.bram.coderswah.controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bram.coderswah.R
 import com.bram.coderswah.Utilities.EXTRA_CATEGORY
+import com.bram.coderswah.adapters.ProductsAdapter
+import com.bram.coderswah.services.DataService
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType+" blbl")
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType!!))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation== Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize> 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/bram/coderswah/services/DataService.kt
+++ app/src/main/java/com/bram/coderswah/services/DataService.kt
@@ -27,6 +27,14 @@ object DataService {
         Product("Devslopes Graphic Beanie","$18", "hat1"),
         Product("Devslopes Hat Black","$20", "hat2"),
         Product("Devslopes Hat White","$18", "hat3"),
+        Product("Devslopes Hat Snapback","$22", "hat4"),
+        Product("Devslopes Graphic Beanie","$18", "hat1"),
+        Product("Devslopes Hat Black","$20", "hat2"),
+        Product("Devslopes Hat White","$18", "hat3"),
+        Product("Devslopes Hat Snapback","$22", "hat4"),
+        Product("Devslopes Graphic Beanie","$18", "hat1"),
+        Product("Devslopes Hat Black","$20", "hat2"),
+        Product("Devslopes Hat White","$18", "hat3"),
         Product("Devslopes Hat Snapback","$22", "hat4")
     )
 
@@ -34,9 +42,27 @@ object DataService {
         Product("Devslopes Hoodie Gray","$28","hoodie1"),
         Product("Devslopes Hoodie Red","$21","hoodie2"),
         Product("Devslopes Gray Hoodie","$30","hoodie3"),
+        Product("Devslopes Black Hoodie","$28","hoodie4"),
+        Product("Devslopes Hoodie Gray","$28","hoodie1"),
+        Product("Devslopes Hoodie Red","$21","hoodie2"),
+        Product("Devslopes Gray Hoodie","$30","hoodie3"),
+        Product("Devslopes Black Hoodie","$28","hoodie4"),
+        Product("Devslopes Hoodie Gray","$28","hoodie1"),
+        Product("Devslopes Hoodie Red","$21","hoodie2"),
+        Product("Devslopes Gray Hoodie","$30","hoodie3"),
         Product("Devslopes Black Hoodie","$28","hoodie4")
     )
     val shirts = listOf(
+        Product("Devslopes Shirt Black","$18","shirt1"),
+        Product("Devslopes Badge Light Gray","$11","shirt2"),
+        Product("Devslopes Logo Shirt Red","$10","shirt3"),
+        Product("Devslopes Hustle","$15","shirt4"),
+        Product("Kickflip Studios","$15","shirt5"),
+        Product("Devslopes Shirt Black","$18","shirt1"),
+        Product("Devslopes Badge Light Gray","$11","shirt2"),
+        Product("Devslopes Logo Shirt Red","$10","shirt3"),
+        Product("Devslopes Hustle","$15","shirt4"),
+        Product("Kickflip Studios","$15","shirt5"),
         Product("Devslopes Shirt Black","$18","shirt1"),
         Product("Devslopes Badge Light Gray","$11","shirt2"),
         Product("Devslopes Logo Shirt Red","$10","shirt3"),
